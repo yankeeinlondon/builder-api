7e1e8bd92c628ab01c607c69419425c26eb7c215
Ken Snyder <ken@ken.net>
2022-07-23 13:16:56 -0700

    chore: initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import Markdown from "vite-plugin-md";
+
+// used for testing, library code uses TSUP to build exports
+export default defineConfig({
+  test: {
+    dir: "test",
+    exclude: ["**/*.spec.ts"],
+    environment: "happy-dom",
+    api: {
+      host: "0.0.0.0",
+    },
+  },
+  plugins: [
+
+    Markdown({
+      // builders: [link(), code()],
+      excerpt: true,
+      exposeExcerpt: true,
+    }),
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+    }) as any,
+  ],
+});

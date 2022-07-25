6e613df31667ad7e035492a54cc0da708bac2a11
Ken Snyder <ken@ken.net>
2022-07-24 21:50:54 -0700

    feat: added RouteConfig, MetaProperty, and LinkProperty to props which are proxied from vite-plugin-md

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type { IPipelineStage, Pipeline, Frontmatter,  HeadProps } from "vite-plugin-md";
+export type { IPipelineStage, Pipeline, Frontmatter, HeadProps, RouteConfig, MetaProperty, LinkProperty } from "vite-plugin-md";
 export { composeSfcBlocks, PipelineStage } from "vite-plugin-md";
 export * from "./types";
 export * from "./createBuilder";
